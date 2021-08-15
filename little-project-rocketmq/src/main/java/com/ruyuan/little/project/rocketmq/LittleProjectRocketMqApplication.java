fb190672f436bd7c11b1e6ab3cce125da487d216
wangzhongyou <964745177@qq.com>
2021-08-15 09:37:52 +0800

    项目初始化

---
+++ little-project-rocketmq/src/main/java/com/ruyuan/little/project/rocketmq/LittleProjectRocketMqApplication.java
@@ -0,0 +1,13 @@
+package com.ruyuan.little.project.rocketmq;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LittleProjectRocketMqApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LittleProjectRocketMqApplication.class, args);
+    }
+
+}
---
+++ little-project-rocketmq/src/main/java/com/ruyuan/little/project/rocketmq/api/HealthController.java
@@ -0,0 +1,15 @@
+package com.ruyuan.little.project.rocketmq.api;
+
+import com.ruyuan.little.project.common.dto.CommonResponse;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HealthController {
+
+    @RequestMapping("/")
+    public CommonResponse health() {
+        return CommonResponse.success();
+    }
+
+}
